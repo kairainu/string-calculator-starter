57cd254c4c6edd03ddbd2b3a9bc004ee42ee768a
Neha Kaira <Sony@DESKTOP-94QUEG5>
2021-10-17 22:29:02 +0530

    Added 5th Test Case: "Negatives not allowed"
    Description: Added a new test cases where if the input string
    contains a negative number, String calculator will throws an
    exception.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,18 +2,21 @@ package calculator;
 
 class StringCalculator {
 	
-    public int add(String input) {
+    public int add(String input) throws Exception {
     	if (input.isEmpty()) {
     		return 0;
     	}
     	if (input.length() == 1) {
     		return toInt(input);
     	}
+    	
     	String delimiter = ",|\n";
+    	
     	if (input.matches("//(.*)\n(.*)")) {
     		delimiter = Character.toString(input.charAt(2));
     		input = input.substring(4);
     	}
+    	
     	String[] numList = input.split(delimiter);
     	return getSum(numList);
     }
@@ -22,7 +25,19 @@ class StringCalculator {
     	return Integer.parseInt(num);
     }
     
-    private int getSum(String[] numList) {
+    private int getSum(String[] numList) throws Exception {
+    	String negativeNumbers = new String();
+        for(String num : numList){
+        	if(toInt(num) < 0){
+        		if(negativeNumbers.length() == 0)
+        			negativeNumbers = num;
+        		else
+        			negativeNumbers += ("," + num);
+        	}
+        }
+        if (negativeNumbers.length() != 0) {
+        	throw new IllegalArgumentException("negatives not allowed - " + negativeNumbers);
+        }
     	int sum = 0;
     	for (String num: numList) {
     		sum += toInt(num);
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,38 +6,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorShould {
 
     @Test
-    void empty_string_should_return_0() {
+    void empty_string_should_return_0() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
+    void string_with_single_number_should_return_number_as_int() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
     
     @Test
-    void string_with_two_numbers_separated_by_comma_delimitor_should_return_their_sum() {
+    void string_with_two_numbers_separated_by_comma_delimitor_should_return_their_sum() throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(5, stringCalculator.add("2,3"));
     }
     
     @Test
-    void string_with_two_numbers_separated_by_newline_delimitor_should_return_their_sum() {
+    void string_with_two_numbers_separated_by_newline_delimitor_should_return_their_sum() throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(10, stringCalculator.add("6\n4"));
     }
     
     @Test
-    void string_with_three_numbers_separated_by_either_delimitor_should_return_their_sum() {
+    void string_with_three_numbers_separated_by_either_delimitor_should_return_their_sum() throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(11, stringCalculator.add("6\n4,1"));
     }
     
     @Test
-    void string_with_two_numbers_separated_by_given_delimitor_should_return_their_sum() {
+    void string_with_two_numbers_separated_by_given_delimitor_should_return_their_sum() throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(3, stringCalculator.add("//;\n1;2"));
+    }
+    
+    @Test
+    void string_with_negative_numbers_should_throw_exception() throws Exception {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	try {
+			stringCalculator.add("-1,2");
+		}
+		catch (IllegalArgumentException e){
+			assertEquals(e.getMessage(), "negatives not allowed - -1");
+		}
+
+		try {
+			stringCalculator.add("2,-4,3,-5");
+		}
+		catch (IllegalArgumentException e){
+			assertEquals(e.getMessage(), "negatives not allowed - -4,-5");
+		}
     }
 }
