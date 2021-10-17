b88953c514d59350648a841946e8ca92d42a71c7
Neha Kaira <Sony@DESKTOP-94QUEG5>
2021-10-17 21:40:52 +0530

    Refactor  the code.
    Description: Make the get sum function a generic one. Removed the overloaded function.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,26 +7,27 @@ class StringCalculator {
     		return 0;
     	}
     	if (input.length() == 1) {
-    		return Integer.parseInt(input);
+    		return toInt(input);
     	}
     	String delimiter = ",|\n";
     	if (input.matches("//(.*)\n(.*)")) {
     		delimiter = Character.toString(input.charAt(2));
     		input = input.substring(4);
     	}
-    	String[] numbers = input.split(delimiter); 
-    	if (numbers.length == 2) {
-    		return getSum(numbers[0], numbers[1]);
+    	String[] numList = input.split(delimiter);
+    	return getSum(numList);
+    }
+    
+    private int toInt(String num) {
+    	return Integer.parseInt(num);
+    }
+    
+    private int getSum(String[] numList) {
+    	int sum = 0;
+    	for (String num: numList) {
+    		sum += toInt(num);
     	}
-    	return getSum(numbers[0], numbers[1] , numbers[2]);
-    }
-    
-    private int getSum(String num1, String num2) {
-    	return Integer.parseInt(num1) + Integer.parseInt(num2);
-    }
-    
-    private int getSum(String num1, String num2, String num3) {
-    	return Integer.parseInt(num1) + Integer.parseInt(num2) + Integer.parseInt(num3);
+    	return sum;
     }
 
 }
